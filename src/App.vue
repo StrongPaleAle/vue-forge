028e061560f585dd2a9275aba71e8620e00fdae8
StrongAle <alessio.falchi90@gmail.com>
2022-07-14 13:20:30 +0100

    single board, drop image and alerts

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheDrawer from "./components/TheDrawer.vue";
-import TheNavbar from "./components/TheNavbar.vue";
+// import TheNavbar from "./components/TheNavbar.vue";
+// import TheAlerts from "./components/TheAlerts.vue";
+// import TheDrawer from "./components/TheDrawer.vue";
 </script>
 
 <template>
   <TheNavbar />
   <TheDrawer />
+  <TheAlerts />
 </template>
