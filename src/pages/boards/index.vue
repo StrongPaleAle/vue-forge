d9c1d66374484929bcafd86c77c6019b33149d01
Sue Holder <susanpholder@gmail.com>
2022-07-17 16:10:53 +0100

    replace hardcoded data

--- src/pages/boards/index.vue
+++ src/pages/boards/index.vue
@@ -1,46 +1,30 @@
 <script setup lang="ts">
 import { useAlerts } from "@/stores/alerts";
 import type { Board } from "@/types";
-import { ref } from "vue";
-const boards = ref<Partial<Board>[]>([
-  {
-    id: "1",
-    title: "My First Board",
-    order: "[]",
-    image: {
-      downloadUrl: "https://picsum.photos/480/270?board=1",
-    },
-  },
-  {
-    id: "2",
-    title: "My Second Board",
-    order: "[]",
-    image: {
-      downloadUrl: "https://picsum.photos/480/270?board=2",
-    },
-  },
-  {
-    id: "3",
-    title: "My Third Board",
-    order: "[]",
-  },
-]);
+import { useQuery } from "@vue/apollo-composable";
+import { ref, computed } from "vue";
+import boardsQuery from "../../graphql/queries/boards.query.gql";
+
+const { result, loading, onError } = useQuery(boardsQuery);
+const boards = computed(() => result.value?.boardsList?.items || []);
 
 const alerts = useAlerts();
 
+onError(() => alerts.error("Error loading boards!"));
 function createBoard() {
   alerts.success("Board created!");
 }
 </script>
 
 <template>
-    <div class="flex items-center justify-between my-5">
-        <h1 class="text-3xl">Boards </h1>
-        <button class="text-gray-500" @click="createBoard()">
-        <span>New Board +</span>
-        </button>
-    </div>
-    <div class="flex">
+  <div class="flex items-center justify-between my-5">
+    <h1 class="text-3xl">Boards</h1>
+    <button class="text-gray-500" @click="createBoard()">
+      <span>New Board +</span>
+    </button>
+  </div>
+  <div class="flex">
     <BoardCard v-for="board in boards" :key="board.id" :board="board" />
-    </div>
+  </div>
+  <p v-if="loading">Loading...</p>
 </template>
