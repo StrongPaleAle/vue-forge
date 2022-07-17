e1b2d4fc2e75331b7f0b9bbf4b02c5d3f59b8ac4
Sue Holder <susanpholder@gmail.com>
2022-07-17 17:27:05 +0100

    added mutation to create a new board

--- src/pages/boards/index.vue
+++ src/pages/boards/index.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { useAlerts } from "@/stores/alerts";
 import type { Board } from "@/types";
-import { useQuery } from "@vue/apollo-composable";
+import { useMutation, useQuery } from "@vue/apollo-composable";
 import { ref, computed } from "vue";
-import boardsQuery from "../../graphql/queries/boards.query.gql";
+import boardsQuery from "@/graphql/queries/boards.query.gql";
+import createBoardMutation from "@/graphql/mutations/createBoard.mutation.gql";
 
 const { result, loading, onError } = useQuery(boardsQuery);
 const boards = computed(() => result.value?.boardsList?.items || []);
@@ -11,15 +12,32 @@ const boards = computed(() => result.value?.boardsList?.items || []);
 const alerts = useAlerts();
 
 onError(() => alerts.error("Error loading boards!"));
-function createBoard() {
-  alerts.success("Board created!");
-}
+
+const { mutate: createBoard } = useMutation(createBoardMutation, () => ({
+  update(cache, { data: { boardCreate } }) {
+    cache.updateQuery({ query: boardsQuery }, (res) => ({
+      boardsList: {
+        items: [...res.boardsList.items, boardCreate],
+      },
+    }));
+  },
+}));
+
+// function createBoard() {
+//   alerts.success("Board created!");
+// }
+
+const newBoardPayload = {
+  data: {
+    title: "Test Board 3",
+  },
+};
 </script>
 
 <template>
   <div class="flex items-center justify-between my-5">
     <h1 class="text-3xl">Boards</h1>
-    <button class="text-gray-500" @click="createBoard()">
+    <button class="text-gray-500" @click="createBoard(newBoardPayload)">
       <span>New Board +</span>
     </button>
   </div>
